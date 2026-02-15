0a01aca9df73b516ad2a81474565bee2d1c06e11
Utsob1621 <utsobsaha9072@gmail.com>
2026-02-16 01:57:03 +0600

    add final task completion notice with timestamp

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 // utilities function
-function getElementById (id) {
+function getElementById(id) {
   return document.getElementById(id);
 }
 
@@ -11,17 +11,20 @@ const datetime = document.createElement("p");
 
 datetime.innerHTML = `
     <p>
-    ${new Date().toLocaleDateString('en-Gb', {
-  weekday: 'long'
-})}
+    ${new Date().toLocaleDateString("en-Gb", {
+      weekday: "long",
+    })}
       <br>
-    <span class = "text-base font-semibold">${new Date().toLocaleDateString('en-Gb', {
-  day: 'numeric',
-  month: 'long',
-  year: 'numeric'
-})}</span> 
+    <span class = "text-base font-semibold">${new Date().toLocaleDateString(
+      "en-Gb",
+      {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      },
+    )}</span> 
     </p>
-  `
+  `;
 dateAndTime.appendChild(datetime);
 
 // task btn
@@ -29,12 +32,12 @@ dateAndTime.appendChild(datetime);
 const taskButtons = document.getElementsByClassName("task-btn");
 let taskDone = document.getElementById("task-done");
 let taskNumber = parseInt(taskDone.innerText);
-let taskAdd = document.getElementById("task-add")
+let taskAdd = document.getElementById("task-add");
 let taskAddNumber = parseInt(taskAdd.innerText);
 const noticeContainer = getElementById("notice-box");
 
 for (let i = 0; i < taskButtons.length; i++) {
-  const taskBtn = taskButtons[i]
+  const taskBtn = taskButtons[i];
 
   taskBtn.addEventListener("click", function (event) {
     event.preventDefault();
@@ -47,11 +50,12 @@ for (let i = 0; i < taskButtons.length; i++) {
 
     if (taskAddNumber > 1) {
       taskAddNumber++;
-      taskAdd.innerText = taskAddNumber
+      taskAdd.innerText = taskAddNumber;
     }
 
-    // notice history 
-    const cardTitles = event.target.parentNode.parentNode.parentNode.children[1].innerText;
+    // notice history
+    const cardTitles =
+      event.target.parentNode.parentNode.parentNode.children[1].innerText;
     const notice = document.createElement("div");
     notice.classList.add("bg-slate-100", "rounded-lg", "p-2", "mb-2");
 
@@ -59,14 +63,28 @@ for (let i = 0; i < taskButtons.length; i++) {
       <p>
         You have Complete The ${cardTitles} at ${new Date().toLocaleTimeString()}
       </p>
-    `
+    `;
     noticeContainer.appendChild(notice);
 
     taskBtn.disabled = true;
-    taskBtn.innerText = "Completed"
-  })
-}
+    taskBtn.innerText = "Completed";
 
+    if (taskNumber === 0) {
+      alert("Congrates!! You have to completed all the current task");
+
+      const finalNotice = document.createElement("div");
+      finalNotice.classList.add("bg-slate-100", "rounded-lg", "p-2", "mb-2");
+
+      finalNotice.innerHTML = `
+      <p>
+        You have complete the task Review Ami Utsob Saha at ${new Date().toLocaleTimeString()}
+      </p>
+    `;
+
+    noticeContainer.appendChild(finalNotice);
+    }
+  });
+}
 
 
 document.getElementById("today-btn").addEventListener("click", function () {
