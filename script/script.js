dd8245db7d4b1be7f63558661d2629d07b830ba1
Utsob1621 <utsobsaha9072@gmail.com>
2026-02-15 14:27:50 +0600

    add dynamic notice history with card title and timestamp

--- script/script.js
+++ script/script.js
@@ -1,21 +1,44 @@
+// utilities function
+function getElementById (id) {
+  return document.getElementById(id);
+}
+
 // task btn
 
 const taskButtons = document.getElementsByClassName("task-btn");
-  let taskDone = document.getElementById("task-done");
-  let taskNumber = parseInt(taskDone.innerText);
-  let taskAdd = document.getElementById("task-add");
+let taskDone = document.getElementById("task-done");
+let taskNumber = parseInt(taskDone.innerText);
+let taskAdd = document.getElementById("task-add")
+let taskAddNumber = parseInt(taskAdd.innerText);
+const noticeContainer = getElementById("notice-box");
 
-for (let i = 0; i < taskButtons.length; i++){
-  const  taskBtn = taskButtons[i]
+for (let i = 0; i < taskButtons.length; i++) {
+  const taskBtn = taskButtons[i]
 
-  taskBtn.addEventListener("click", function(event){
+  taskBtn.addEventListener("click", function (event) {
     event.preventDefault();
     alert("Board updated successfully");
-    
-    if(taskNumber > 0 ){
+
+    if (taskNumber > 0) {
       taskNumber--;
       taskDone.innerText = taskNumber;
     }
 
+    if (taskAddNumber > 1) {
+      taskAddNumber++;
+      taskAdd.innerText = taskAddNumber
+    }
+
+    // notice history 
+    const cardTitles = event.target.parentNode.parentNode.parentNode.children[1].innerText;
+    const notice = document.createElement("div");
+    notice.classList.add("bg-slate-100", "rounded-lg", "p-2", "mb-2");
+
+    notice.innerHTML = `
+      <p>
+        You have Complete The ${cardTitles} at ${new Date().toLocaleTimeString()}
+      </p>
+    `
+    noticeContainer.appendChild(notice);
   })
 }
